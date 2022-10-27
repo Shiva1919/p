82f759b6b9ad35826244b619f844e2b57afad97d
Vikram Window <Vikram@Window.com>
2022-10-27 18:31:22 +0530

    conditioan change hsn controllers

--- app/Http/Controllers/hsnController.php
+++ app/Http/Controllers/hsnController.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\DB;
 class hsnController extends Controller
 {
    function index($id){
-    $query =  DB::connection('mysql_2')->table('HsnCodesTable')->where('hsncode', $id)->get();
 
-    if ($query) {
+    $query =  DB::connection('mysql_2')->table('HsnCodesTable')->where('hsncode', $id)->get();
+// return count($query);
+
+    if (count($query)) {
         return response()->json(['data'=>$query[0],'status'=> 1]);
     }
     else{
