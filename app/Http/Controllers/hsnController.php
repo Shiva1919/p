59ee98662af386ae79f85f08b929132cab84b693
Vikram Window <Vikram@Window.com>
2022-11-07 17:09:30 +0530

    hsn controller changes

--- app/Http/Controllers/hsnController.php
+++ app/Http/Controllers/hsnController.php
@@ -7,19 +7,12 @@ use Illuminate\Support\Facades\DB;
 class hsnController extends Controller
 {
    function index($id){
-
     $query =  DB::connection('mysql_2')->table('HsnCodesTable')->where('hsncode', $id)->get();
-// return count($query);
-
     if (count($query)) {
         return response()->json(['data'=>$query[0],'status'=> 1]);
     }
     else{
-
         return response()->json(['status'=> 0]);
-
     }
-
-
    }
 }
